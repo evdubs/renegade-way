c4d7af2d33f2f628a93794064c41d37a2a7e8231
evdubs <1124030+evdubs@users.noreply.github.com>
2022-02-14 15:30:38 -1000

    add logging

---
+++ logging.rkt
@@ -0,0 +1,18 @@
+#lang racket/base
+
+(require gregor)
+
+(provide start-logging)
+
+(define (start-logging)
+  (define log (make-logger #f #f 'info #f))
+  (current-logger log)
+  (define rec (make-log-receiver log 'info))
+
+  (define out (open-output-file (string-append "/var/tmp/renegade-way/" (~t (today) "yyyy-MM") ".log")
+                                #:exists 'append))
+  (file-stream-buffer-mode out 'line)
+
+  (thread (λ ()
+            (do ([log-event (sync rec) (sync rec)]) (#f)
+              (displayln (string-append (~t (now) "yyyy-MM-dd hh:mm:ss") " " (vector-ref log-event 1)) out)))))
